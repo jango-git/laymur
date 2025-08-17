74fb7d8c2edc92057bbdba9bcac3f5e505c94bcf
Jango <jango@jango.pro>
2025-08-17 21:21:14 +0200

    Uncomment UI nine-slice implementation

--- src/elements/UINineSline.ts
+++ src/elements/UINineSline.ts
@@ -1,41 +1,41 @@
-// import type { Texture, WebGLRenderer } from "three";
-// import { Mesh } from "three";
-// import type { UILayer } from "../layers/UILayer";
-// import { UINineSliceMaterial } from "../materials/UINineSliceMaterial";
-// import { geometry } from "../miscellaneous/threeInstances";
-// import { UIElement } from "./UIElement";
+import type { Texture, WebGLRenderer } from "three";
+import { Mesh } from "three";
+import type { UILayer } from "../layers/UILayer";
+import { UINineSliceMaterial } from "../materials/UINineSliceMaterial";
+import { geometry } from "../miscellaneous/threeInstances";
+import { UIElement } from "./UIElement";
 
-// export class UINineSlice extends UIElement<Mesh> {
-//   private readonly material: UINineSliceMaterial;
-//   private readonly textureInternal: Texture;
+export class UINineSlice extends UIElement<Mesh> {
+  private readonly material: UINineSliceMaterial;
+  private readonly textureInternal: Texture;
 
-//   constructor(layer: UILayer, texture: Texture, x = 0, y = 0) {
-//     const w = texture.image.width;
-//     const h = texture.image.height;
+  constructor(layer: UILayer, texture: Texture, x = 0, y = 0) {
+    const w = texture.image.width;
+    const h = texture.image.height;
 
-//     const material = new UINineSliceMaterial(texture);
-//     const object = new Mesh(geometry, material);
+    const material = new UINineSliceMaterial(texture);
+    const object = new Mesh(geometry, material);
 
-//     super(layer, x, y, w, h, object);
+    super(layer, x, y, w, h, object);
 
-//     this.material = material;
-//     this.textureInternal = texture;
-//   }
+    this.material = material;
+    this.textureInternal = texture;
+  }
 
-//   public override destroy(): void {
-//     this.material.dispose();
-//     super.destroy();
-//   }
+  public override destroy(): void {
+    this.material.dispose();
+    super.destroy();
+  }
 
-//   protected override ["onBeforeRenderInternal"](
-//     renderer: WebGLRenderer,
-//     deltaTime: number,
-//   ): void {
-//     void renderer;
-//     void deltaTime;
-//     this.material.setQuadSize(
-//       this.width * this.micro.scaleX,
-//       this.height * this.micro.scaleY,
-//     );
-//   }
-// }
+  protected override ["onBeforeRenderInternal"](
+    renderer: WebGLRenderer,
+    deltaTime: number,
+  ): void {
+    void renderer;
+    void deltaTime;
+    this.material.setQuadSize(
+      this.width * this.micro.scaleX,
+      this.height * this.micro.scaleY,
+    );
+  }
+}
